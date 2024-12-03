f342121387fa6063102dc5c0f1e8c9dbc8009a70
Tejas Kale <tejas.kale@cctech.co.in>
2024-12-03 15:55:35 +0530

    Add project files.

---
+++ Coding_Exercise_17/The_Finally_Keyword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Coding_Exercise_17
+{
+    internal class The_Finally_Keyword
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Trying...");
+            }
+            finally
+            {
+                Console.WriteLine("Finally executed.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
